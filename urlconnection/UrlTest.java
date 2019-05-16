1f5ef82e6ee65b3002bf51bc570615d44c0a6d03
kis <kis@pc01>
2019-05-16 23:06:44 +0900

    ...

---
+++ urlconnection/UrlTest.java
@@ -0,0 +1,27 @@
+import java.net.*; 
+import java.io.*; 
+
+public class UrlTest{ 
+    public static void main(String args[])  
+                             throws MalformedURLException, IOException
+    { 
+        URL url = new URL("http://www.naver.com");  
+        URLConnection conn = url.openConnection(); 
+        //출력 스트림 제어 
+        conn.setDoOutput(true); 
+        OutputStream os = conn.getOutputStream(); 
+        PrintWriter out = new PrintWriter(os); 
+        out.println("qt=love");  
+        out.close(); 
+        //입력 스트림 제어 
+        String temp; 
+        InputStream is = conn.getInputStream(); 
+        InputStreamReader isr = new InputStreamReader(is); 
+        BufferedReader br = new BufferedReader(isr); 
+        while( (temp=br.readLine())!=null)
+        { 
+            System.out.println(temp); 
+        } 
+        br.close(); 
+    } 
+} 
