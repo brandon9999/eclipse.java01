07b3fce97164d8a87e9e822d270e3821fcef7d29
brandon9999 <induomo@gmail.com>
2021-06-11 10:40:11 +0900

    1

--- BasicAPI/Static/StaticErr.java
+++ BasicAPI/Static/StaticErr.java
@@ -2,12 +2,15 @@
 
 public class StaticErr 
 {   
-	private int s;   
+	private static int s;   
+	//private int s;   
+	
 	//스태틱 멤버 메서드에서 일반 멤버 변수 사용(에러)   
 	public static void printStaticData()
 	{        
 		System.out.println(s); //에러
 	}    
+	
 	public static void main(String[] args)
 	{       
 		StaticErr.printStaticData();  
